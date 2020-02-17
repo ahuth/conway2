fa492a76fd3a297fe8bed029afbff8a11c2e9fbe
Andrew Huth <andrew@huth.me>
2020-02-16 22:03:43 -0800

    Clean the dist/ directory before building

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,8 +16,9 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: "Conway",
+      title: 'Conway',
     }),
   ],
 };
