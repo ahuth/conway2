11c37ee7659b3545eef332a7f6bec537401c7b05
Andrew Huth <andrew@huth.me>
2020-02-17 14:36:33 -0800

    Use a webpack plugin to cleanup the build directories

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -15,6 +16,7 @@ module.exports = {
   },
 
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Conway2',
     }),
