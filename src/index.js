5457d1f0161c4d0a7fbcf0ce122b2c0cc4f7b27e
Andrew Huth <andrew@huth.me>
2020-02-16 22:56:58 -0800

    Load and use our webassembly function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('hello world');
+
+const importObject = {
+  env: {
+    abort: () => {},
+  },
+};
+
+WebAssembly.instantiateStreaming(fetch('optimized.wasm'), importObject).then(function (module) {
+  const exports = module.instance.exports;
+  console.log('2 + 3:', exports.add(2, 3));
+});
