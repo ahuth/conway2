89fa9915fe9510cd8b3a2df99872d4542cffc562
Andrew Huth <andrew@huth.me>
2020-02-17 21:37:38 -0800

    Get a reference to the canvas

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
 import './index.css';
 
-console.log('hello world');
+window.addEventListener('DOMContentLoaded', function () {
+  const canvas = document.getElementById('canvas');
+
+  if (!(canvas instanceof HTMLCanvasElement)) {
+    return;
+  }
+
+  const context = canvas.getContext('2d');
+  console.log(context);
+});
