74b89bb6805ea081bde596a1a0434635d4ec2afb
Andrew Huth <andrew@huth.me>
2020-02-18 20:52:26 -0800

    Correct setting colors values in ImageData
    Each color component is a separate index, which makes sense.

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,26 @@ window.addEventListener('DOMContentLoaded', function () {
 
   const context = canvas.getContext('2d')!;
   const { height, width } = canvas.getBoundingClientRect();
-
-  const size = height * width;
   const imageData = context.createImageData(width, height);
 
-  for (let index = 0; index < size; index += 1) {
-    imageData.data[index] = Math.random() > 0.2 ? 255 : 0;
+  for (let x = 0; x < width; x += 1) {
+    for (let y = 0; y < height; y += 1) {
+      const [redIndex, greenIndex, blueIndex, alphaIndex] = getColorIndicesForCoord(x, y, width);
+      const on = Math.random() > 0.2;
+      imageData.data[redIndex] = on ? 255 : 0;
+      imageData.data[greenIndex] = on ? 255 : 0;
+      imageData.data[blueIndex] = on ? 255 : 0;
+      imageData.data[alphaIndex] = 255;
+    }
   }
 
   context.putImageData(imageData, 0, 0);
 });
+
+function getColorIndicesForCoord(x: number, y: number, width: number) {
+  const red = y * (width * 4) + x * 4;
+  const green = red + 1;
+  const blue = red + 2;
+  const alpha = red + 3;
+  return [red, green, blue, alpha];
+}
