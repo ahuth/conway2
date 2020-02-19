ce4f02231f627e79bb9b0425592591a469860005
Andrew Huth <andrew@huth.me>
2020-02-18 20:35:17 -0800

    Randomly initialize the canvas

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import './index.css';
 
 window.addEventListener('DOMContentLoaded', function () {
-  const canvas = document.getElementById('canvas');
+  const canvas = document.getElementById('canvas')!;
 
   if (!(canvas instanceof HTMLCanvasElement)) {
     return;
   }
 
-  const context = canvas.getContext('2d');
-  console.log(context);
+  const context = canvas.getContext('2d')!;
+  const { height, width } = canvas.getBoundingClientRect();
+
+  const size = height * width;
+  const imageData = context.createImageData(width, height);
+
+  for (let index = 0; index < size; index += 1) {
+    imageData.data[index] = Math.random() > 0.2 ? 255 : 0;
+  }
+
+  context.putImageData(imageData, 0, 0);
 });
