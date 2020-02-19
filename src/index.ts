a7fd6b70f88ac1859b73a18e272d057671c55d84
Andrew Huth <andrew@huth.me>
2020-02-18 21:01:57 -0800

    Use function to set color on the ImageData

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import './index.css';
 
 window.addEventListener('DOMContentLoaded', function () {
@@ -13,17 +14,21 @@ window.addEventListener('DOMContentLoaded', function () {
 
   for (let x = 0; x < width; x += 1) {
     for (let y = 0; y < height; y += 1) {
-      const [redIndex, greenIndex, blueIndex, alphaIndex] = getColorIndicesForCoord(x, y, width);
       const on = Math.random() > 0.2;
-      imageData.data[redIndex] = on ? 255 : 0;
-      imageData.data[greenIndex] = on ? 255 : 0;
-      imageData.data[blueIndex] = on ? 255 : 0;
-      imageData.data[alphaIndex] = 255;
+      setRgb(imageData, x, y, on ? 255 : 0, on ? 255 : 0, on ? 255 : 0);
     }
   }
 
   context.putImageData(imageData, 0, 0);
 });
+
+function setRgb(imageData: ImageData, x: number, y: number, red: number, green: number, blue: number) {
+  const [redIndex, greenIndex, blueIndex, alphaIndex] = getColorIndicesForCoord(x, y, imageData.width);
+  imageData.data[redIndex] = red;
+  imageData.data[greenIndex] = green;
+  imageData.data[blueIndex] = blue;
+  imageData.data[alphaIndex] = 255;
+}
 
 function getColorIndicesForCoord(x: number, y: number, width: number) {
   const red = y * (width * 4) + x * 4;
